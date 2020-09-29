f56abf5d40767ec8f576413459c39a00ebeeadc6
moT01 <20648924+moT01@users.noreply.github.com>
2020-09-29 09:48:01 -0500

    init

---
+++ RPS.py
@@ -0,0 +1,10 @@
+# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+
+def player(prev_play, opponent_history=[]):
+    opponent_history.append(prev_play)
+
+    guess = "R"
+    if len(opponent_history) > 2:
+        guess = opponent_history[-2]
+
+    return guess
