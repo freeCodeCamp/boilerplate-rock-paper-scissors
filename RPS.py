5134725e3e380ca373d7845556edd7e2ba44dc2d
adamstok <adamstokowski@outlook.com>
2021-03-18 13:21:39 +0100

    markov v1

--- RPS.py
+++ RPS.py
@@ -1,32 +1,65 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 import random
 
+guessed = []
+
 
 def player(prev_play, opponent_history=[]):
+    global guessed
+    third = 1 / 3
     opponent_history.append(prev_play)
-    guess = "R"
-    if len(opponent_history) >= 3:
-        df = {}
-        for i in range(2, len(opponent_history)):
-            key = "".join([str(x) for x in opponent_history[i - 2 : i]])
-            if key not in df:
-                df[key] = [opponent_history[i]]
-            else:
-                df[key].append(opponent_history[i])
 
-    last_key = opponent_history[-2:]
-    if last_key in opponent_history:
-        probR = 100 * (df[last_key].count("R") / len(df[last_key]))
-        probP = 100 * (df[last_key].count("P") / len(df[last_key]))
-        probS = 100 * (df[last_key].count("S") / len(df[last_key]))
+    markov_df = {}
+    # NEXT BOT OUTPUT   R   ,   P  ,   S
+    markov_df["PR"] = [third, third, third]
+    markov_df["PP"] = [third, third, third]
+    markov_df["PS"] = [third, third, third]
+    markov_df["RR"] = [third, third, third]
+    markov_df["RP"] = [third, third, third]
+    markov_df["RS"] = [third, third, third]
+    markov_df["SR"] = [third, third, third]
+    markov_df["SP"] = [third, third, third]
+    markov_df["SS"] = [third, third, third]
 
-        max_probability = max(probR, probP)
-        max_probability = max(max_probability, probS)
-        if max_probability == probR:
-            guess = "P"
-        elif max_probability == probP:
-            guess = "S"
-        elif max_probability == probS:
+    if len(guessed) <= 4:
+        guess = random.choice(["R", "P", "S"])
+        guessed.append(guess)
+    elif len(opponent_history) >= 3:
+        # print(guessed[-1], opponent_history[-2])
+        for i in range(len(opponent_history) - 1):
+            key = guessed[i] + opponent_history[i]
+            if len(key) < 2:
+                key = guessed[i] * 2
+            next_move = opponent_history[i + 1]
+            if next_move == "R":
+                markov_df[key][1] += third
+                markov_df[key][0] -= third
+                markov_df[key][2] -= third
+            elif next_move == "P":
+                markov_df[key][2] += third
+                markov_df[key][1] -= third
+                markov_df[key][0] -= third
+            elif next_move == "S":
+                markov_df[key][0] += third
+                markov_df[key][1] -= third
+                markov_df[key][2] -= third
+        # print(markov_df[key])
+
+        last_key = guessed[-2] + opponent_history[-1]
+        if len(last_key) < 2:
+            last_key = guessed[-2] * 2
+        probR = markov_df[last_key][0]
+        probP = markov_df[last_key][1]
+        probS = markov_df[last_key][2]
+        maxed_prob = max(probR, probP)
+        maxed_prob = max(maxed_prob, probS)
+
+        if maxed_prob == probR:
             guess = "R"
+        elif maxed_prob == probP:
+            guess = "P"
+        elif maxed_prob == probS:
+            guess = "S"
 
+        guessed.append(guess)
     return guess
