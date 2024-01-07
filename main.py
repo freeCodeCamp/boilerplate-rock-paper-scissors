271c2b3fe0c23f84e6afd51eff949e78557934e6
Flin199019 <155882403+Flin199019@users.noreply.github.com>
2024-01-07 13:37:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ play(player, kris, 1000)
 play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
-# play(human, abbey, 20, verbose=True)
+play(human, abbey, 20, verbose=True)
 
 # Uncomment line below to play against a bot that plays randomly:
-# play(human, random_player, 1000)
+play(human, random_player, 1000)
 
 
 
 # Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
